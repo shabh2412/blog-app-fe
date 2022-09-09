1944d984cbc45de986fdb89b373facdc42798904
Rishabh Panesar <rishabhpanesar@gmail.com>
2022-09-10 03:29:14 +0530

    created post blog page

---
+++ src/Pages/Blogs/PostBlog.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PostBlogForm from "../../Components/Blogs/PostBlogForm";
+
+type Props = {};
+
+const PostBlog = (props: Props) => {
+	return (
+		<div>
+			<PostBlogForm />
+		</div>
+	);
+};
+
+export default PostBlog;
